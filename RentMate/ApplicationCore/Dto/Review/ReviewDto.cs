3f4e60c8d6e428ce2c7e2a7fb0f36d8737e42bff
MichalWojciechowsk2 <michal.wojciechowsk2@microsoft.wsei.edu.pl>
2025-11-23 23:02:23 +0100

    Rewiew Controller, Service, Repo

---
+++ RentMate/ApplicationCore/Dto/Review/ReviewDto.cs
@@ -0,0 +1,16 @@
+﻿using Data.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace ApplicationCore.Dto.Review
+{
+    public class ReviewDto
+    {
+        public int Id { get; set; }
+        public int? PropertyId { get; set; }
+        public int? UserId { get; set; }
+        [Range(1, 5)]
+        public decimal Rating { get; set; }
+        [StringLength(1000)]
+        public string Comment { get; set; }
+    }
+}
--- RentMate/RentMateApi/Program.cs
+++ RentMate/RentMateApi/Program.cs
@@ -81,6 +81,9 @@ namespace RentMateApi
             builder.Services.AddScoped<IChatService, ChatService>();
             builder.Services.AddScoped<IChatRepository, ChatRepository>();
 
+            builder.Services.AddScoped<IReviewService, ReviewService>();
+            builder.Services.AddScoped<IReviewRepository, ReviewRepository>();
+
             //builder.Services.AddScoped<IIssueService, IssueService>();
             //builder.Services.AddScoped<IIssueRepository, IssueRepository>();
 
